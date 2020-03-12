0c10564ba573f70002b0f0a60312d948ed204f7e
arenuzzz <syn.max.13@gmail.com>
2020-03-12 14:06:03 +0200

    set new config

---
+++ src/sum.ts
@@ -0,0 +1,5 @@
+import { Sum1, Sum2, SumResult } from './sum.types';
+
+export function sum(a: Sum1, b: Sum2): SumResult {
+  return a + b;
+}
---
+++ src/sum.types.ts
@@ -0,0 +1,3 @@
+export type Sum1 = number;
+export type Sum2 = number;
+export type SumResult = number;
