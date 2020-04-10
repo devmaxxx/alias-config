84db22325d087491157607d08285f73df2ea3054
arenuzzz <syn.max.13@gmail.com>
2020-04-10 19:14:14 +0300

    add createAliases to export

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,4 @@
 export { parseConfig } from './core/parseConfig';
+export { createAliases } from './core/createAliases';
 export { createWebpackAliases } from './createWebpackAliases';
 export { createJestAliases } from './createJestAliases';
